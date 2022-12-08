fa31c81fe0bfe2be726d1a5c0e41f7835210f4e8
rafaelovna <aikanush777@mail.ru>
2022-12-08 18:47:01 +0400

    Задание выполнено!

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,22 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача 1");
-        for (int i=1; i<=10; i++)
+        for (int i = 1; i <= 10; i++)
             System.out.println("Итерация цикла " + i);
     }
     public static void task2() {
             System.out.println("Задача 2");
-
-        for (int b=10; b>=1; b--)
+        for (int b = 10; b >= 1; b--)
             System.out.println("Итерация цикла " + b);
     }
     public static void task3() {
             System.out.println("Задача 3");
-
-        for (int n=0; n<=17; n=n+2)
+        for (int n = 0; n <= 17; n = n + 2)
             System.out.println("Итерация цикла " + n);
     }
     public static void task4() {
             System.out.println("Задача 4");
-
-        for (int r=10; r>=-10; r--)
+        for (int r = 10; r >= -10; r--)
             System.out.println("Итерация цикла " + r);
     }
 }
